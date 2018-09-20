011d328abfba2e164ccb3c90d52e2f3a3bb58c4a
Emilio Bernal <ebernalzubieta@gmail.com>
2018-09-19 21:10:54 -0500

    Added Hero placeholder

---
+++ main.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */ 
+new TypeIt('#htext', {
+    speed: 100,
+    breakLines: true
+})
+.type("Hey! I'm Emilio Bernal")
+.pause(300)
+.break()
+.type("And I'm a programmer")
+.break()
+.type("What can I do for you?");
